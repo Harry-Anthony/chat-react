8b7e9f776faebb06e01281416d67f9cb1587ca77
Fidy Andriamihaja <fidy.andriamihaja@wiz.fr>
2024-07-20 01:03:53 +0300

    package config

--- src/data/remote_config.ts
+++ src/data/remote_config.ts
@@ -1,4 +1,4 @@
-let baseUrl = 'http://localhost:3001/chat-harivola/';
+let baseUrl = 'https://chat-back-2928.onrender.com/chat-harivola/';
 export class RemoteConfig {
     static login = baseUrl + 'auth/login';
     static register = baseUrl + 'auth/register';
